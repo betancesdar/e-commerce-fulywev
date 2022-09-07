f5c188a930456527e85af1ad040ef9c1329b072d
Gerald J Astcio <jerard_17@hotmail.com>
2022-09-07 18:14:02 -0400

    Actualizacion

--- js/carrito.js
+++ js/carrito.js
@@ -7,11 +7,13 @@ class Carrito {
     e.preventDefault();
     if (e.target.classList.contains("agregar-carrito")) {
       const producto = e.target.parentElement.parentElement;
-
-      this.leerDatosProducto(producto);
+console.log(producto)
+      //this.leerDatosProducto(producto);
     }
   }
-  leerDatosProducto(producto) {
+
+
+  /*leerDatosProducto(producto) {
     const infoProducto = {
 
       titulo: producto.querySelector('h3').textContent,
@@ -48,7 +50,7 @@ class Carrito {
     listaProductos.appendChild(row);
 
 
-  }
+  }*/
 
 }
 
